98c82900d9ff072adbc45f2d5119fea47826f5db
sudo-phantom <sudophantom@gmail.com>
2021-02-05 01:38:08 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt install -y software-properties-common ;\
 sudo apt install -y cmake pkg-config libglib2.0-dev libgpgme-dev libgnutls28-dev uuid-dev libssh-gcrypt-dev \
 libldap2-dev doxygen graphviz libradcli-dev libhiredis-dev libpcap-dev bison libksba-dev libsnmp-dev \
 gcc-mingw-w64 heimdal-dev libpopt-dev xmltoman redis-server xsltproc libical-dev postgresql \
-postgresql-contrib postgresql-server-dev-all gnutls-bin nmap rpm nsis curl wget fakeroot gnupg \
+postgresql-contrib postgresql-server-dev-all gnutls-bin nmap rpm nsis curl wget fakerootsource  gnupg \
 sshpass socat snmp smbclient libmicrohttpd-dev libxml2-dev python-polib gettext rsync xml-twig-tools \
 python3-paramiko python3-lxml python3-defusedxml python3-pip python3-psutil virtualenv vim git ;\
 sudo apt install -y texlive-latex-extra --no-install-recommends ;\
